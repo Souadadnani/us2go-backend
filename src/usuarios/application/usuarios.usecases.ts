154996253fd87bc036839537ccdaa0638e2e050d
Souad <220113@fppiramide.com>
2024-03-04 08:42:45 +0100

    a

--- src/usuarios/application/usuarios.usecases.ts
+++ src/usuarios/application/usuarios.usecases.ts
@@ -3,7 +3,12 @@ import UsuariosRepository from "../domain/usuarios.repository";
 
 export default class UsuariosUseCases {
    constructor(private usariosRepository: UsuariosRepository){}
+
    async login(usuario: Usuario){
 
+   }
+
+   async registro(usuario: Usuario){
+      
    }
 }
